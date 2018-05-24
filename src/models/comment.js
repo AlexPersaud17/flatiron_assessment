87180955330f717a6bc3a5500ab12ac7b53ae6dc
Alex Persaud <alex_persaud01@yahoo.com>
2018-05-23 21:20:20 -0400

    Create comment class with required initialized variables and functions

--- src/models/comment.js
+++ src/models/comment.js
@@ -1,6 +1,20 @@
 // create Comment class here
 class Comment{
   constructor(comment, imageId){
+    this.commentContent = comment
+    this.imageObj = this.findImage(imageId)
+    this.id = this.all().length
+  }
+
+  all(){
+    
+  }
+
+  findImage(imageId){
+
+  }
+
+  commentEl(){
 
   }
 
