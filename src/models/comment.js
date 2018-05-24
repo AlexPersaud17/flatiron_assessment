6e4340a2c296d27c6ba38372f4d74567f6cd2f11
Alex Persaud <alex_persaud01@yahoo.com>
2018-05-23 22:05:32 -0400

    Change class format woops :D

--- src/models/comment.js
+++ src/models/comment.js
@@ -1,25 +1,22 @@
-// create Comment class here
-class Comment{
-  constructor(comment, imageId){
-    this.commentContent = comment
-    this.imageObj = this.findImage(imageId)
-    this.id = this.all().length
-  }
+function Comment(comment, imageId){
+  this.commentContent = comment
+  this.imageObj = this.findImage(imageId)
+  this.id = this.constructor.all.length;
+  this.constructor.all.push(this);
+  debugger
+}
 
-  all(){
-    return this.imageObj.comments
-  }
+Comment.all = []
 
-  findImage(imageId){
-    this.imageObj = Image.all.find(function(image){
-      return image.id === imageId
-    })
-    this.imageObj.comments.push(this)
-    return this.imageObj
-  }
+Comment.prototype.findImage = function(imageId){
+  this.imageObj = Image.all.find(function(image){
+    return image.id === imageId
+  })
+  this.imageObj.comments.push(this)
+  return this.imageObj
 
-  commentEl(){
-    return `<li id="image-${this.imageObj.id}-comment-${this.id}">${this.commentContent}</li>`
-  }
+}
 
+Comment.prototype.commentEl = function(){
+  return `<li id="image-${this.imageObj.id}-comment-${this.id}">${this.commentContent}</li>`
 }
